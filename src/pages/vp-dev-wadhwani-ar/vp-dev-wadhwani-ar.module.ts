7e9b4f29d8b2e4061648cb65782c657933ee231f
Ralph Mabeza <alpmabeza@gmail.com>
2017-06-28 15:03:16 +0800

    Fixed Dev's AR module, applied right to left for AR pages.

--- src/pages/vp-dev-wadhwani-ar/vp-dev-wadhwani-ar.module.ts
+++ src/pages/vp-dev-wadhwani-ar/vp-dev-wadhwani-ar.module.ts
@@ -1,21 +1,16 @@
-import { Component, ViewChild } from '@angular/core';
-import { IonicPage, Content, NavController, NavParams } from 'ionic-angular';
-import { TheSpeakersPageAR } from '../thespeakers-ar/thespeakers-ar';
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { VPdwPageAR } from '../vp-dev-wadhwani-ar/vp-dev-wadhwani-ar';
 
-@Component({
-  selector: 'page-vp',
-  templateUrl: 'vp-dev-wadhwani.html-ar'
+@NgModule({
+  declarations: [
+    VPdwPageAR,
+  ],
+  imports: [
+    IonicPageModule.forChild(VPdwPageAR),
+  ],
+  exports: [
+    VPdwPageAR
+  ]
 })
-export class VPdwPageAR {
-  TheSpeakersPageAR = TheSpeakersPageAR;
-
-  @ViewChild(Content) content: Content;
-
-  scrollToTop() {
-    this.content.scrollToTop();
-  }
-  constructor(public navCtrl: NavController) {
-
-  }
-
-}
+export class VPdwPageARModule {}
